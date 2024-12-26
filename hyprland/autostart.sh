ab581003a1a1a78423f0c0526b14ba626b7701c5
RamiMohamed12 <roomierami00@gmail.com>
2024-12-26 11:28:10 +0000

    added hyprland config

---
+++ hyprland/autostart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+hyprpaper & 
+
+sleep 2
+
+
+# Set the wallpaper
+hyprctl hyprpaper wallpaper "HDMI-A-1,/home/demy/Pictures/wallpaper.png"
+
+hyprctl hyprpaper wallpaper "eDP-1,/home/demy/Pictures/wallpaper.png"
+
+# Add more commands below if needed
+
